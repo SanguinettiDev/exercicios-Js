c3785368e519b496e9cf33bdb7f93e12eb1f8fde
Bernardo Sanguinetti <bernardo.serrano.sanguinetti@gmail.com>
2025-04-15 18:23:44 -0300

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,8 +1,8 @@
-function sumOfSquares(nums) {
+function somaDeQuadrados(nums) {
     return nums
         .map(num => num * num)
         .reduce((acc, square) => acc + square, 0);
 }
 
 const nums = [1, 2, 3, 4];
-console.log(sumOfSquares(nums)); 
+console.log(somaDeQuadrados(nums)); 
