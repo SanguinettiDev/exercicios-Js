ceab3d1b77380d95fb0dd43b0353f079d31d4951
Bernardo Sanguinetti <bernardo.serrano.sanguinetti@gmail.com>
2025-04-15 18:20:15 -0300

    03.js

--- 03.js
+++ 03.js
@@ -1,12 +1,11 @@
-function fizzBuzz(n) {
+function cocaCola(n) {
     return Array.from({ length: n }, (_, i) => {
         const num = i + 1;
-        if (num % 15 === 0) return "FizzBuzz";
-        if (num % 3 === 0) return "Fizz";
-        if (num % 5 === 0) return "Buzz";
+        if (num % 15 === 0) return "cocaCola";
+        if (num % 3 === 0) return "Coca";
+        if (num % 5 === 0) return "Cola";
         return num;
     });
 }
 
-console.log(fizzBuzz(15));
-// Saída: [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+console.log(cocaCola(15));
